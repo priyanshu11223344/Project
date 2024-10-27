bcb4cca833346489b8d52a4f08c3e5fa579a6f17
priyanshu11223344 <priyanshupanchal2105@gmail.com>
2024-10-27 13:21:04 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-  origin: 'https://project-1-frontend-indol.vercel.app ' // Allow requests from this origin
+  origin: ['https://project-1-frontend-indol.vercel.app '],
+  methods:["POST","GET"] // Allow requests from this origin
 }));
 app.use(express.text({ type: 'application/xml' }));
 
