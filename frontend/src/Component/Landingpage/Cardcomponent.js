a76efa9daeab078d0a7e7ba093e1ad3aeb84fbaf
priyanshu11223344 <priyanshupanchal2105@gmail.com>
2024-10-28 16:27:47 +0530

    done

--- frontend/src/Component/Landingpage/Cardcomponent.js
+++ frontend/src/Component/Landingpage/Cardcomponent.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import hotelimg from '../../images/hd2.jpg'
+
 const Cardcomponent = (props) => {
   return (
     <div>
       <div className="card" style={{ width: '18rem' }}>
-
-  <img src={props.image} className="card-img-top" height="250px" width="100px"/>
-  <div className="card-body">
-    <h4 className="card-title">{props.title}</h4>
-    <h5 className="card-title">{props.description}</h5>
-    {/* <h5 className="card-title">{props.additionalData}</h5> */}
-    <h5 className='"card-title'>{props.price}</h5>
-    <p className="card-text">{props.additionalData}</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item">{props.Special}</li>
-    <li className="list-group-item">{props.remark}</li>
-    <button className='btn btn-primary'>BOOK</button>
-  </ul>
-  <div className="card-body">
-    <a href="#" className="card-link" >Card link</a>
-    <a href="#" className="card-link">Another link</a>
-  </div>
-</div>
+        <img src={props.image} className="card-img-top" height="250px" width="100px"/>
+        <div className="card-body">
+          <h4 className="card-title">{props.title}</h4>
+          <h5 className="card-title">{props.description}</h5>
+          <h5 className="card-title">{props.price}</h5>
+          <p className="card-text">{props.additionalData}</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">{props.Special}</li>
+          <li className="list-group-item">{props.remark}</li>
+          <button
+            className="btn btn-primary"
+            onClick={() => props.handleBookClick(props.hotelDetails)} // Pass hotel details on click
+          >
+            BOOK
+          </button>
+        </ul>
+        <div className="card-body">
+          <a href="#" className="card-link">Card link</a>
+          <a href="#" className="card-link">Another link</a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cardcomponent
+export default Cardcomponent;
--- frontend/src/Component/Landingpage/HotelOffers.js
+++ frontend/src/Component/Landingpage/HotelOffers.js
@@ -7,32 +7,29 @@ import { Audio } from 'react-loader-spinner';
 const HotelOffers = () => {
     const context = useContext(Hotelcontext);
     const { fetchData, data } = context;
-    // useEffect(() => {
-    //     const storedCheckin = localStorage.getItem('checkin');
-    //      // Fetch data on component mount if needed
-    //      if(storedCheckin){
-    //         fetchData(storedCheckin)
-    //      }
-    // }, []);
-    console.log("data is", data); 
+
+    // Helper functions
     const stripHtml = (html) => {
         const div = document.createElement('div');
         div.innerHTML = html;
         return div.textContent || div.innerText || '';
     };
 
-    // Function to truncate remark to 50 words
     const truncateRemark = (remark, wordLimit = 20) => {
         const words = remark.split(' ');
         if (words.length > wordLimit) {
-            return words.slice(0, wordLimit).join(' ') + '...'; // Truncate and add ellipsis
+            return words.slice(0, wordLimit).join(' ') + '...';
         }
-        return remark; // Return the full remark if it's within the limit
+        return remark;
+    };
+
+    const handleBookClick = (hotelDetails) => {
+        console.log("Hotel details:", hotelDetails); // Logs the details of the clicked hotel
     };
 
     return (
         <div>
-            {data?.Hotels?.length > 0 ? ( // Check if Hotels array exists and has items
+            {data?.Hotels?.length > 0 ? (
                 <div className="hotelmap">
                     {data.Hotels.map((hotel, index) => (
                         <Cardcomponent
@@ -44,6 +41,8 @@ const HotelOffers = () => {
                             additionalData={`Category: ${hotel.Offers?.[0]?.Category || 'N/A'}, Rooms: ${hotel.Offers?.[0]?.Rooms?.join(', ') || 'N/A'}`}
                             Special={`Special: ${stripHtml(hotel.Offers?.[0]?.Special || "NONE")}`}
                             remark={`Remark: ${truncateRemark(stripHtml(hotel.Offers?.[0]?.Remark || ''))}`}
+                            handleBookClick={handleBookClick} // Pass handleBookClick function
+                            hotelDetails={hotel} // Pass entire hotel object for detailed logging
                         />
                     ))}
                 </div>
@@ -60,8 +59,6 @@ const HotelOffers = () => {
                 </div>
             )}
         </div>
-
-
     );
 };
 
